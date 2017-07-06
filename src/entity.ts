3c78f1943eb37f238cb12af9da04425db93e2d2f
Joel Grenon <joel.grenon@ampliflex.com>
2017-07-06 10:24:41 -0400

    Add debugging trace to understand signature issue

--- src/entity.ts
+++ src/entity.ts
@@ -235,9 +235,9 @@ export default class Entity {
 
         // verify the signatures (for both assertion/message)
         if (!libsaml.verifySignature(res, {
-            cert: opts.from.entityMeta,
-            signatureAlgorithm: opts.from.entitySetting.requestSignatureAlgorithm,
-          })) {
+          cert: opts.from.entityMeta,
+          signatureAlgorithm: opts.from.entitySetting.requestSignatureAlgorithm,
+        })) {
           throw new Error('incorrect signature');
         }
       }
@@ -249,6 +249,7 @@ export default class Entity {
         extract: libsaml.extractor(res, fields),
       };
       if (checkSignature && from.entitySetting.messageSigningOrder === messageSigningOrders.SIGN_THEN_ENCRYPT) {
+        console.log("Verifying signature: ", res, opts.from.entityMeta, opts.from.entitySetting.requestSignatureAlgorithm);
         // verify the signatures (for both assertion/message)
         if (!libsaml.verifySignature(res, {
           cert: opts.from.entityMeta,
